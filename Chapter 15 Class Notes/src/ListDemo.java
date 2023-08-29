588f57346d73a3de66af30117418007f64d4a87e
Geoff Schmit <gcschmit@gmail.com>
2023-08-29 09:19:21 -0500

    start notes on LinkedList

--- Chapter 15 Class Notes/src/ListDemo.java
+++ Chapter 15 Class Notes/src/ListDemo.java
@@ -9,6 +9,17 @@ public class ListDemo
 {
     public static void main(String[] args)
     {
+        /*
+         * The addLast method can be used to populate a list.
+         */
+        LinkedList<String> staff = new LinkedList<>();
+        staff.addLast("Diana");
+        staff.addLast("Harry");
+        staff.addLast("Romeo");
+        staff.addLast("Tom");
+
+        // The list is currently: DHRT
+
         
     }
 }
