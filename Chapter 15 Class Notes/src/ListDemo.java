15756ff6a134fb15e9ae4636664c03f07232d3c9
Geoff Schmit <gcschmit@gmail.com>
2023-08-30 10:26:32 -0500

    add notes for list iterator methods

--- Chapter 15 Class Notes/src/ListDemo.java
+++ Chapter 15 Class Notes/src/ListDemo.java
@@ -19,7 +19,63 @@ public class ListDemo
         staff.addLast("Tom");
 
         // The list is currently: DHRT
+        /*
+         * The listIterator method create a new list iterator that is
+         *  positioned at the front of the list.
+         * The "|" in the comment represents the iterator position.
+         */
+        ListIterator<String> iterator = staff.listIterator(); // |DHRT
 
+        /*
+         * The next method advances the iterator over the next element
+         *  in the list.
+         */
+        iterator.next();    // D|HRT
+
+        /*
+         * The next method also returns the element that the iterator
+         *  is passing.
+         */
+        String name = iterator.next();      // DH|RT
+        System.out.println(name);
+        System.out.println("Expected: Harry");
+
+        /*
+         * The add method inserts an element at the iterator position.
+         * The iterator is then positioned after the element that was added.
+         *  (The text is a bit confusing on this point....)
+         */
+        iterator.add("Juliet");     // DHJ|RT
+        iterator.add("Nina");       // DHJN|RT
+
+        /*
+         * The remove method removes the element returned by the last
+         *  call to next or previous.
+         * The remove method can only be called once after calling next
+         *  or previous.
+         * The remove method cannot be called after calling add.
+         */
+        iterator.next();            // DHJNR|T
+        iterator.remove();          // DHJN|T
+
+        System.out.println(staff);
+        System.out.println("Expected: [Diana, Harry, Juliet, Nina, Tom]");
+
+        /*
+         * The set method updates the element returned by the last call
+         *  to next or previous.
+         */
+        iterator.previous();          // DHJ|NT
+        iterator.set("Albert");     // DHJ|AT
         
+
+
+
+
+
+
+
+
+
     }
 }
