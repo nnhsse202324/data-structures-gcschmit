5a0e6b060308af6c4d6163934db5c7e015537270
Geoff Schmit <gcschmit@gmail.com>
2023-09-15 07:34:05 -0500

    add notes on maps

--- Chapter 15 Class Notes/src/MapDemo.java
+++ Chapter 15 Class Notes/src/MapDemo.java
@@ -11,5 +11,30 @@ public class MapDemo
 {
     public static void main(String[] args)
     {
+        /*
+         * The Map interface is a generic. The first type is the type
+         *  of the key; the second, the type of the value.
+         */
+        Map<String, Color> favoriteColors = new HashMap<>();
+
+        favoriteColors.put("Connor", Color.BLUE);
+        favoriteColors.put("Angela", Color.PINK);
+
+        // two different keys can have the same value
+        favoriteColors.put("Rohan", Color.BLUE);
+
+        favoriteColors.put("Roham", Color.RED);
+
+        // but the same key cannot have two different values
+        // this code changes the value for the key "Angela"
+        favoriteColors.put("Angela", Color.YELLOW);
+
+        // create a set of the keys in a map
+        Set<String> keys = favoriteColors.keySet();
+        for(String key : keys)
+        {
+            System.out.println(key + " (" + key.hashCode() + "): " +
+                favoriteColors.get(key));  
+        }
     }
 }
