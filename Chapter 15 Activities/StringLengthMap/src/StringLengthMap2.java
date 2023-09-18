6d3fbdf7ac04c95368fb12790e7ad2de49a5caa0
Geoff Schmit <gcschmit@gmail.com>
2023-09-18 10:50:44 -0500

    add notes on lambda expressions

--- Chapter 15 Activities/StringLengthMap/src/StringLengthMap2.java
+++ Chapter 15 Activities/StringLengthMap/src/StringLengthMap2.java
@@ -1,5 +1,6 @@
 import java.util.*;
 import java.io.*;
+
 /**
  * Read all words from a file and add them to a
  * map whose keys are word lengths and whose values
@@ -8,46 +9,108 @@ import java.io.*;
  * the length of their entries.
  * Use the Java 8 merge() method from Java 8 Note 15.1.
  */
-public class StringLengthMap2
-{
-    public static void main(String[] args)
-    {
+public class StringLengthMap2 {
+    public static void main(String[] args) {
         String filename = "src/test1.txt";
 
-        try (Scanner in = new Scanner(new File(filename)))
-        {
+        try (Scanner in = new Scanner(new File(filename))) {
 
             // Create your map here
-            
+            Map<Integer, String> words = new TreeMap<>();
 
-            while (in.hasNext())
-            {
+            while (in.hasNext()) {
                 String word = clean(in.next());
                 Integer len = word.length();
 
-                // Update the map here
-                // Use the Java 8 merge() method
-                
+                /*
+                 * Below is my sample solution of the first part of the activity.
+                 */
 
+                /*
+                 * String oldValue = words.get(len);
+                 * String newValue = word;
+                 * String str;
+                 * 
+                 * if (oldValue != null) {
+                 * str = newValue;
+                 * } else {
+                 * str = oldValue + ", " + newValue;
+                 * }
+                 * words.put(len, str);
+                 */
 
+                /*
+                 * The same functionality can be applied by using the merge method
+                 * of the Map class. The first parameter to the
+                 * merge method is the key; the second, the value if there is no
+                 * association already in the map; the third, a reference to a method.
+                 * 
+                 * The "::" operator returns a reference to the specified method.
+                 * 
+                 * This method must take two parameters; the first, the value currently
+                 * in the map; the second, the value to merge with the value already in the map.
+                 * The method must return the "merged" value to be added to the map. The method
+                 * is only invoked if there is already an association in the map and, therefore,
+                 * the values have to be "merged".
+                 */
+                words.merge(len, word, StringLengthMap2::remappingFunction);
+
+                /*
+                 * Instead of defining the remappingFunction method, which is only
+                 * referenced when invoking the merge method, we can use a Java
+                 * lambda expression to define the method "inline".
+                 * 
+                 * The parameters to the method are specified in the parentheses without
+                 * types as the types are implicit based on the merge method. The method
+                 * definition follows the "->" operator. If the definition consists of a
+                 * single line, curly brackets are not needed and the return keyword is
+                 * implied.
+                 */
+                words.merge(len, word,
+                        (oldValue, newValue) -> oldValue.concat(", " + word));
+
+                /*
+                 * If the definition spans multiple statements, curly brackets are
+                 *  required and the value must be explicitly returned.
+                 */
+                words.merge(len, word,
+                        (oldValue, newValue) -> {
+                            if (oldValue.indexOf(word) == -1)
+                                return oldValue.concat(", " + word);
+                            else
+                                return oldValue;
+                        });
+            }
+
+            for (Integer len : words.keySet()) {
+                System.out.println(len + ": " + words.get(len));
             }
 
             // Print the strings, in increasing order of their length
             // Use this format: 1: i, a, i
-        } catch (FileNotFoundException e)
-        {
+        } catch (FileNotFoundException e) {
             System.out.println("Cannot open: " + filename);
         }
     }
 
-    public static String clean(String s)
-    {
+    /**
+     * This method "merges" the newValue with the oldValue such that the "merged"
+     * value can be added to the map, replacing the old value
+     * 
+     * @param oldValue the value currently associated in the map
+     * @param newValue the value to "merge" with the value currently associated in
+     *                 the map
+     * @return the "merged" value to replace the value currently in the map
+     */
+    private static String remappingFunction(String oldValue, String newValue) {
+        return oldValue.concat(", " + newValue);
+    }
+
+    public static String clean(String s) {
         String r = "";
-        for (int i = 0; i < s.length(); i++)
-        {
+        for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
-            if (Character.isLetter(c))
-            {
+            if (Character.isLetter(c)) {
                 r = r + c;
             }
         }
