f06d926a5f7cef3cda474c122f1c043cd809affe
Derek J Miller <DJMiller@Naperville203.org>
2023-09-21 13:40:38 -0500

    Finished Set Lesson

--- Chapter 15 Class Notes/src/WordAnalysis.java
+++ Chapter 15 Class Notes/src/WordAnalysis.java
@@ -13,6 +13,31 @@ public class WordAnalysis
     public static void main(String[] args)
         throws FileNotFoundException
     {
+        // Read the dictionary and novel
+        Set<String> dictionaryWords = readWords("Chapter 15 Class Notes/src/words");
+        Set<String> novelWords = readWords("Chapter 15 Class Notes/src/throughTheLookingGlass.txt");
+        
+        // Print all the words from the novel, not in the dictionary
+        for (String word: novelWords) {
+            if (!dictionaryWords.contains(word)) {
+                System.out.println(word);
+            }
+        }
+
+        // Print the number of unique words in the novel
+        System.out.println("There are "+novelWords.size()+" unique words in the novel.");
+
+        // Print the number of unique words with >3 letters
+        Iterator<String> iterator = novelWords.iterator();
+        while (iterator.hasNext()) {
+            if (iterator.next().length() <= 3) {
+                iterator.remove();
+            }
+        }
+        System.out.println("There are "+novelWords.size()+" unique words with more than three letters in the novel.");
+
+    
+        
     }
 
     /**
@@ -25,6 +50,18 @@ public class WordAnalysis
     public static Set<String> readWords(String filename)
         throws FileNotFoundException
     {
-        return null;
+        Set<String> words = new HashSet<>();
+       // System.out.println(System.getProperty("user.dir"));
+        Scanner in = new Scanner(new File(filename), "UTF-8");
+        
+        // Use any character other than letters as delimeters
+        in.useDelimiter("[^a-zA-Z]+");
+
+        // Adding words to our set (duplicates are ignored)
+        while (in.hasNext()) {
+            words.add(in.next().toLowerCase());
+        }
+
+        return words;
     }
 }
